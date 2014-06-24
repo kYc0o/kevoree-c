714564c0dbc29724b6c69b86690d99b4dd97173b
kYc0o <francisco.acosta@inria.fr>
2014-06-24 15:40:59 +0200

    Relocate source files because of complilation problems

--- README.md
+++
@@ -1,4 +0,0 @@
-kevoree-contiki
-===============
-
-This is a Contiki implementation for the Kevoree component model for STM32F4 nodes.
--- realloc.h
+++
@@ -1,7 +0,0 @@
-#ifndef H_realloc
-#define H_realloc
-#include <stdlib.h>
-
-size_t getsize(void *p);
-void *realloc(void *ptr, size_t size);
-#endif
