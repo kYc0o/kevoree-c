8c0ecc34d5450b4bcd74d0a11183d58f149948b6
kYc0o <fco.ja.ac@gmail.com>
2015-02-17 15:07:17 +0100

    Added KMF4C

---
+++ src/KMF4C.h
@@ -0,0 +1,33 @@
+#ifndef __KMF4C_H
+#define __KMF4C_H
+
+#include <stdbool.h>
+
+typedef struct _KMFContainer KMFContainer;
+typedef struct _Visitor Visitor;
+
+typedef char* (*fptrKMFMetaClassName)(void*);
+typedef char* (*fptrKMFInternalGetKey)(void*);
+/*
+ * TODO fix to single visitor function pointer
+ */
+typedef void (*fptrVisitAttr)(void*, char*, Visitor*, bool);
+typedef void (*fptrVisitRefs)(void*, char*, Visitor*, bool);
+typedef void* (*fptrFindByPath)(char*, void*);
+typedef void (*fptrDelete)(void*);
+
+typedef struct _KMFContainer {
+	void *pDerivedObj;
+	char *eContainer;
+	fptrKMFMetaClassName metaClassName;
+	fptrKMFInternalGetKey internalGetKey;
+	fptrVisitAttr VisitAttributes;
+	fptrVisitAttr VisitPathAttributes;
+	fptrVisitRefs VisitReferences;
+	fptrVisitRefs VisitPathReferences;
+	fptrFindByPath FindByPath;
+	fptrDelete Delete;
+} KMFContainer;
+
+
+#endif
